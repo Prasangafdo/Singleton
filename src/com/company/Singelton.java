48662647c603f3ff817a6db9ab3fa4ee2a34f361
150101002 <prasangafdz@outlook.com>
2017-06-03 14:32:09 +0530

    Working

--- src/com/company/Singelton.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-/**
- * Created by 150101002 on 6/3/2017.
- */
-public class Singelton {
-}
---
+++ src/com/company/Singleton.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by 150101002 on 6/3/2017.
+ */
+public class Singleton {
+   private int setX;
+
+    public void setX (int x){
+        this.setX = x;
+    }
+    public int getX (){
+        return setX;
+}
+}
