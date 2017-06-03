b0a41e9570d55c8b3a98cf4a602cb942231b23d7
150101002 <prasangafdz@outlook.com>
2017-06-03 14:36:01 +0530

    Edited by sir

--- src/com/company/Singleton.java
+++ src/com/company/Singleton.java
@@ -4,12 +4,24 @@ package com.company;
  * Created by 150101002 on 6/3/2017.
  */
 public class Singleton {
-   private int setX;
+    private  static  Singleton singleton = new Singleton();
+    private  Singleton(){
+
+    }
+
+    public static Singleton getSingleton() {
+        return singleton;
+    }
+
+    private int x;
 
     public void setX (int x){
-        this.setX = x;
+        this.x = x;
     }
     public int getX (){
-        return setX;
+        return x;
+}
+public void add(int val){
+        x +=val;
 }
 }
